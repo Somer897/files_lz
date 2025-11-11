801d4e80da7e243ce36a02cf42636e87efa6adff
Somer897 <tyme897@gmail.com>
2025-11-11 17:18:03 +0300

    1

--- lion.py
+++ lion.py
@@ -1,11 +1,14 @@
 import docx
-import matplotlib
+import matplotlib.pyplot as plt
 
 #открытие файла
 doc = docx.Document('lion.docx')
 
 #где будет находиться текст
 text = []
+
+#списокдля букв
+letters = {}
 
 #чтение текста
 for paragraph in doc.paragraphs:
@@ -14,8 +17,47 @@ for paragraph in doc.paragraphs:
 #разбиение на абзацы
 full_text = '\n'.join(text)
 
-#счетчик слова
-word = str(input('какое ваше слово(буква): '))
-kol = full_text.count(word)
+#перевод букв на строчный тип
+full_text = full_text.lower()
 
-print(kol)
+#удаление ненужных символов
+punc = '/?!.,"«»[](){}-–:;—_1234567á890äâѵ°ȃ=ôęï―ç…όù*îö№üóûòêèàé’'
+for i in range(0, len(punc)) :
+    if punc[i] in full_text: 
+        full_text = full_text.replace(punc[i], '')
+
+
+#счетчик слов
+#задаем искомое слово
+word = str(input('какое ваше слово: '))
+#находим количество слов
+kol = full_text.count(word)
+#вычисляем процент использвания
+percent = ((kol / len(full_text))*100)
+
+#цикл для нахождения букв в тексте
+#"влезаем" в слова в тексте
+for i in full_text:
+    #а теперь из слов в буквы
+    for j in i:
+        #если есть буква в словаре, то счетчик +1
+        if j in letters:
+            letters[j] += 1
+        #если буквы нету, то добавляет ее в словарь
+        else:
+            letters.update({j:1})
+
+#создание осей для графика
+keys = list(letters.keys())
+values = list(letters.values())
+
+#создаем график через бар, т.к. он создаёт НУЖНЫЙ график для нас, в отличии от plot
+plt.bar(keys, values)
+
+#называем оси и сам график
+plt.xlabel("буквы")
+plt.ylabel("Количество")
+plt.title("Гистограмма количества букв")
+
+#показ графика
+plt.show()
